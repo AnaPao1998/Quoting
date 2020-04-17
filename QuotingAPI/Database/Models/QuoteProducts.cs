73a10ed3b2630cbaf98005efdc7207c9902a6755
AndresGB <andresgamboabaldi@gmail.com>
2020-04-17 19:57:09 -0400

    Created Database Models

---
+++ QuotingAPI/Database/Models/QuoteProducts.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuotingAPI.Database.Models
+{
+    public class QuoteProducts
+    {
+        public string QuoteCode { get; set; }
+        public string ProductCode { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}

c9040dcdf0691e09519a47844086f49fbd375105
Rodrigo Heredia <rodrigoh1205@gmail.com>
2020-04-18 13:43:56 -0400

    DTO Models added

---
+++ QuotingAPI/DTOModels/QuoteDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuotingAPI.DTOModels
+{
+    public class QuoteDTO
+    {
+        public string QuoteName { get; set; }
+        public List <QuoteProductsDTO> QuoteLineItems { get; set; }
+    }
+}

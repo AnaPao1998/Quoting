fd5700cf586a15434263b4ab22403285fb11fb1a
Andrea Villarroel <anditavillarroel99@gmail.com>
2020-04-18 17:10:10 -0400

    Adding Business Logic

---
+++ QuotingAPI/BusinessLogic/IQuotesLogic.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using QuotingAPI.DTOModels;
+
+namespace QuotingAPI.BusinessLogic
+{
+    public interface IQuotesLogic
+    {
+        public List<QuoteDTO> GetQuoteList();
+       
+    }
+}

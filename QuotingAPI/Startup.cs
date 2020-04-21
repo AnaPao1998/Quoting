204f446454c961cb3c1dad0abed0a51ae1e6bad1
Rodrigo Heredia <rodrigoh1205@gmail.com>
2020-04-20 20:00:52 -0400

    Swagger added

--- QuotingAPI/Startup.cs
+++ QuotingAPI/Startup.cs
@@ -30,7 +30,11 @@ namespace QuotingAPI
             services.AddControllers();
             services.AddTransient<IQuotesLogic, QuotesLogic>();
             services.AddTransient<IQuoteListDB, QuoteListDB>();
-      
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo() { Title = "Group Selector API - DEV/QA", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,6 +54,12 @@ namespace QuotingAPI
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Group Selector");
             });
         }
     }
